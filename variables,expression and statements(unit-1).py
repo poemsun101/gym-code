f4b059b7ca23e5d90152c891e177da8a78fcbaea
kavita kumari <119148361+poemsun101@users.noreply.github.com>
2022-12-09 19:34:57 +0530

    Add files via upload

---
+++ variables,expression and statements(unit-1).py
@@ -0,0 +1,36 @@
+# variables,expression and statements Python String Concatenation
+var1="Hello world"
+var4="hello sun"
+var5="36"
+var6="20"
+var2=4
+var3=25.3
+print(var1)
+print(var2)
+print(var3)
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(var2+var3)
+print(var1+var4)
+print(var1+var4+var5)
+a="hello"
+b="world"
+c=a + " " + b
+print(c)
+print(var5+var6)
+print(int(var5)+int(var6))
+print(str(var5)+str(var6))
+print(float(var5)+float(var6))
+print(10*"hello world")
+print(10*"hello world\n")
+print(10*int(var5)+int(var6))
+print(10*str(int(var5)+int(var6)))
+print("Enter your number")
+inpnum=input()
+print("you entered",int(inpnum)+10)
+print("Enter first number")
+n1=input()
+print("Enter second number")
+n2=input()
+print("sum of these two numbers is",int(n1)+int(n2))
